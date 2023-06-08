4ec0255ecd9215bcb969cb66943f5a065a65180d
mask ser <appushijil72@gmail.com>
2023-06-08 20:34:32 +0530

    edit

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Set up static file serving
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set views directory and view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Define routes
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/test', (req, res) => {
+  res.render('pages/index');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log();
+});
